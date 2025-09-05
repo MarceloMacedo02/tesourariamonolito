bd417733c6a1264b4bac170182b4a297f28ceb3b
marce <marcelo.macedo01@hotmail.com>
2025-09-05 10:30:49 -0300

    1

--- src/main/java/br/com/sigest/tesouraria/repository/GrupoMensalidadeRepository.java
+++ src/main/java/br/com/sigest/tesouraria/repository/GrupoMensalidadeRepository.java
@@ -1,4 +1,9 @@
 package br.com.sigest.tesouraria.repository;
 import br.com.sigest.tesouraria.domain.entity.GrupoMensalidade;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface GrupoMensalidadeRepository extends JpaRepository<GrupoMensalidade, Long> {}
+
+import java.util.Optional;
+
+public interface GrupoMensalidadeRepository extends JpaRepository<GrupoMensalidade, Long> {
+    Optional<GrupoMensalidade> findByNome(String nome);
+}
