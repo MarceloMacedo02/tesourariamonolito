b2eb8780b41d6d600388255fe09901a72576bfd3
marce <marcelo.macedo01@hotmail.com>
2025-09-07 07:20:58 -0300

    2

--- src/main/java/br/com/sigest/tesouraria/domain/entity/Transacao.java
+++ src/main/java/br/com/sigest/tesouraria/domain/entity/Transacao.java
@@ -1,12 +1,20 @@
 package br.com.sigest.tesouraria.domain.entity;
 
-import br.com.sigest.tesouraria.domain.enums.Lancado;
-import br.com.sigest.tesouraria.domain.enums.TipoTransacao;
-import jakarta.persistence.*;
-import lombok.Data;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
+
+import br.com.sigest.tesouraria.domain.enums.Lancado;
+import br.com.sigest.tesouraria.domain.enums.TipoTransacao;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Data;
 
 @Entity
 @Table(name = "transacoes")
